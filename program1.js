d5ca9a93e595cb66270a3ed553a7cd7bd481b88a
ChasGustaf <gustaf.sjonell@chasacademy.se>
2024-09-12 14:14:24 +0200

    Add files via upload

---
+++ program1.js
@@ -0,0 +1,15 @@
+//alert("hello world");
+
+//console.log("hej värld");
+
+//document.write("hej på dig<br>");
+
+//document.write("pingvin<br>")
+
+document.body.innerHTML = "lite text";
+
+document.body.innerHTML += "egen test";
+
+document.body.innerHTML += "ost";
+
+//document.write("tomater")
